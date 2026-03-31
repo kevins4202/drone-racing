0b1fe066b9b49654e5206d75044f9f4896fc287d
Kevin Song <song_zj@hotmail.com>
2026-03-31 14:32:28 +0000

    std

--- src/isaac_quad_sim2real/tasks/race/config/crazyflie/agents/rsl_rl_ppo_cfg.py
+++ src/isaac_quad_sim2real/tasks/race/config/crazyflie/agents/rsl_rl_ppo_cfg.py
@@ -21,7 +21,7 @@ class QuadcopterPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         actor_hidden_dims=[256, 256],       # was [128, 128]; more capacity for 25d obs
         critic_hidden_dims=[512, 512, 256], # updated for 34d privileged critic obs
         activation="elu",
-        min_std=0.0,
+        min_std=0.01,
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
